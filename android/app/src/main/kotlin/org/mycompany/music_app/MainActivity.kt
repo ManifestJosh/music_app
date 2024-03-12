13b8d99ec1878bf94376c9860c924fd6ee7c8d19
ManifestJosh <okeugochijoshua@gmail.com>
2024-03-12 03:48:48 +0100

    new app

---
+++ android/app/src/main/kotlin/org/mycompany/music_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.mycompany.music_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/Drawer.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Center(
+              child: Icon(
+                Icons.music_note,
+                size: 40,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 25),
+            child: ListTile(
+              title: Text("H O M E"),
+              leading: Icon(Icons.home),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
